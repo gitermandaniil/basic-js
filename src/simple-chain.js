ddda79a44b53f9bacbc94916623755317c46c4a0
Daniil Giterman <giterman@corpcentre.ru>
2020-09-24 01:37:06 +0400

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,35 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+  chain: [],
 
-module.exports = chainMaker;
+  getLength () {
+    return this.chain.length
+  },
+
+  addLink (value) {
+    this.chain.push(`( ${value} )`)
+    return this
+  },
+
+  removeLink (position) {
+    if (!this.chain[position]) {
+      this.chain = []
+      throw new Error()
+    }
+
+    this.chain.splice(position - 1, 1)
+    return this
+  },
+
+  reverseChain () {
+    this.chain.reverse()
+    return this
+  },
+
+  finishChain () {
+    const result = this.chain.join('~~')
+    this.chain = []
+    return result
+  }
+}
+
+module.exports = chainMaker
