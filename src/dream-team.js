f27988272c94656ca859711fc51fd9dd49a6abe2
Daniil Giterman <giterman@corpcentre.ru>
2020-09-23 22:49:01 +0400

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam (members) {
+  if (!Array.isArray(members)) return false
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  return members.map(member => {
+    if (typeof member === 'string') {
+      return member.trim()[0].toUpperCase()
+    }
+  }).sort().join('')
+}
