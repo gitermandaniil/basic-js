a3881e1c0efae29f7527ee97533c8728f2f31132
Daniil Giterman <giterman@corpcentre.ru>
2020-09-23 22:49:58 +0400

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform (arr) {
+  if (!Array.isArray(arr)) throw new Error()
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  const transformed = []
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++
+        break
+
+      case '--discard-prev':
+        if (transformed.length && arr[i - 2] !== '--discard-next') transformed.pop()
+        break
+
+      case '--double-next':
+        if (i < arr.length - 1) transformed.push(arr[i + 1])
+        break
+
+      case '--double-prev':
+        if (i && arr[i - 2] !== '--discard-next') transformed.push(arr[i - 1])
+        break
+
+      default:
+        transformed.push(arr[i])
+    }
+  }
+
+  return transformed
+}
